e3d09b3282509241afb3670a40d2572d016b9d9b
ErikDanielsson <danielsson.erik.0@gmail.com>
2026-02-26 15:09:19 +0100

    Revert get rate after conversion error

--- to-mc.sh
+++ to-mc.sh
@@ -1,1 +1,1 @@
-build/tpplc models/host-repertoire-evolution/simple-HRM.tppl --output build/temp --print-model > ../miking-dppl/$1.mc
+build/tpplc-graph models/host-repertoire-evolution/simple-HRM.tppl --output build/temp --print-model > ../miking-dppl/$1.mc
