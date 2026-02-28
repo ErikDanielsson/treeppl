6abab954b8fea78670e5a49464707b50264f5d5c
ErikDanielsson <danielsson.erik.0@gmail.com>
2026-02-28 17:10:39 +0100

    Add ref printing and update model

--- to-mc.sh
+++ to-mc.sh
@@ -1,1 +1,12 @@
-build/tpplc-graph models/host-repertoire-evolution/simple-HRM.tppl --output build/temp --print-model > ../miking-dppl/$1.mc
+print_model () {
+    model=$1
+    echo "Printing model $1"
+    td=$2
+    ref=$(git rev-parse --short HEAD)
+    build/tpplc-graph $model --output build/temp --print-model > $td/$(basename $1 .tppl).$ref.mc
+}
+
+model_dir=models/host-repertoire-evolution
+target_dir=../miking-dppl
+print_model $model_dir/flat-root-prior-HRM.tppl $target_dir/flat-root-prior-HRM
+print_model $model_dir/subroot-HRM.tppl $target_dir/subroot-HRM
