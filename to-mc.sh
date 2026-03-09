2b026c03356547d9775549da8edd5d0d511ba83d
ErikDanielsson <danielsson.erik.0@gmail.com>
2026-03-09 20:15:19 +0100

    Update simulation with fixed initial repertoire and better tracking of changes

--- to-mc.sh
+++ to-mc.sh
@@ -1,12 +1,19 @@
+model_dir=models/host-repertoire-evolution
+lib_dir=$model_dir/host-rep-lib
 print_model () {
     model=$1
     echo "Printing model $1"
     td=$2
-    ref=$(git rev-parse --short HEAD)
-    build/tpplc-graph $model --output build/temp --print-model > $td/$(basename $1 .tppl).$ref.mc
+    ref=$(git rev-parse --short $(git rev-list -1 HEAD -- $model $lib_dir))
+    target=$td/$(basename $1 .tppl).$ref.mc
+    if [ -f $target ]; then
+        echo "Target $target exists, skipping"
+    else
+        echo "Target $target does not exists, skipping"
+        build/tpplc-graph $model --output build/temp --print-model > $target
+    fi
 }
 
-model_dir=models/host-repertoire-evolution
 target_dir=../miking-dppl
 print_model $model_dir/flat-root-prior-HRM.tppl $target_dir/flat-root-prior-HRM
 print_model $model_dir/subroot-HRM.tppl $target_dir/subroot-HRM
